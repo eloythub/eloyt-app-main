75791a8a5e5e5d63b32b4b34b3f19c0531af2df4
Mahan <mahan@gomeeki.com>
2017-02-27 11:59:11 +0700

    refactor the name style

--- Eloyt/index.android.js
+++ Eloyt/index.android.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import Bootstrap from 'Eloyt/js/bootstrap';
+import Bootstrap from 'Eloyt/Js/Bootstrap';
 
 AppRegistry.registerComponent('Eloyt', () => Bootstrap);
