f5a1be353034b86b894c09680cfbf84750b50aea
Mahan <mahan@gomeeki.com>
2017-03-01 20:07:37 +0700

    redux and router flux

---
+++ Eloyt/Js/Components/Scenes/Login/LoginScene.js
@@ -0,0 +1,20 @@
+import React, { Component, PropTypes } from 'react';
+import { Text, View } from 'react-native';
+
+export default class LoginScene extends Component {
+  static contextTypes = {
+    routes: PropTypes.object.isRequired,
+  };
+
+  render() {
+    const {routes} = this.context;
+
+    return (
+      <View>
+        <Text>Login Scene</Text>
+      </View>
+    );
+  }
+}
+
+export const LoginSceneKey = 'login';
---
+++ Eloyt/Js/Reducers.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+// Import Components Scenes Reducers here
+import routes from 'Eloyt/Js/Routes';
+
+export default combineReducers({
+  routes,
+});
