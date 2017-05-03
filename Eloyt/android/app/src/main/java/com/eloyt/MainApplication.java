16e1df71eade518121dbd3e0a900e785776089bb
Mahan <mahan@gomeeki.com>
2017-05-03 16:10:48 +0700

    install camera component for ios

--- Eloyt/android/app/src/main/java/com/eloyt/MainApplication.java
+++ Eloyt/android/app/src/main/java/com/eloyt/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.rnfs.RNFSPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -42,6 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new RCTCameraPackage(),
             new ReactVideoPackage(),
             new RNFSPackage(),
         new LinearGradientPackage(),
