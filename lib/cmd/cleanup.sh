c7344d3f302d2e64e63214951b8eeda108e1eabb
Mahan <mahan@gomeeki.com>
2017-08-15 16:12:50 +0700

    update

--- lib/cmd/cleanup.sh
+++ lib/cmd/cleanup.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-watchman watch-del-all 
-rm -rf node_modules
-rm -rf yarn.lock 
-rm -rf $TMPDIR/react-packager-*
-rm -rf ios/build
-npm cache clear
-#yarn
+watchman watch-del-all 1>/dev/null
+rm -rf node_modules 1>/dev/null
+rm -rf yarn.lock  1>/dev/null
+rm -rf package-lock.json 1>/dev/null
+rm -rf $TMPDIR/react-packager-* 1>/dev/null
+rm -rf ios/build 1>/dev/null
+npm cache clear --force -s 1>/dev/null
+npm cache verify 1>/dev/null
+
+
 yarn
+yarn add react-native-fbsdk@0.6.0
 react-native link
