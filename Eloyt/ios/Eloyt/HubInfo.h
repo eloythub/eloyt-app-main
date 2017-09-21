46f78d5ea59f847e5ffa566ff8e1638c567841de
Mahan <mahan@gomeeki.com>
2017-09-21 21:39:07 +0700

    hubinfo.h

---
+++ Eloyt/ios/Eloyt/HubInfo.h
@@ -0,0 +1,8 @@
+#ifndef HubInfo_h
+#define HubInfo_h
+
+#define HUBNAME @"eloythub"
+#define HUBLISTENACCESS @"Endpoint=sb://eloythub.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=F4CwJz/oT5fKxqd0U7RdqcbtJvFAIcTrqSBInwZV9tE="
+
+#endif /* HubInfo_h */
+
