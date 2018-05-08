032c5b132df9c2771249fcb4ed032ac24ec590ac
Mahan <mahan@gomeeki.com>
2018-05-08 23:22:08 +0700

    fix

--- Eloyt/js/libs/controller.js
+++ Eloyt/js/libs/controller.js
@@ -5,7 +5,6 @@ export default class Controller extends React.Component {
   navigate (method, scene) {
     log('navigate', method, scene)
 
-    console.log(this.props.navigation)
     this.props.navigation[method](scene)
   }
 }
